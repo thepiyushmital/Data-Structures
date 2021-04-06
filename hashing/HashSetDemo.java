3ed5cfdc06850c1cd833289f986c76f50b07adcd
GLOBAL\pmital <piyushmital1996@gmail.com>
2021-04-06 21:11:22 +0530

    HASH SET CLASS

---
+++ hashing/HashSetDemo.java
@@ -0,0 +1,39 @@
+import java.util.*;	
+
+class HashSetDemo{
+
+	// Data type should be non-primitive ie; String, Integer, etc
+	
+	static HashSet<String> hashSet = new HashSet<String>();
+	
+	public static void main(String args[]){
+		/*
+		Main functions
+		1. clear()		O(1)
+		2. isEmpty()	O(1)
+		3. add()		O(1) on avg
+		4. remove()		O(1) on avg
+		5. size()		O(1) 
+		6. contains()  	O(1) on avg
+		*/
+		
+		hashSet.add("Mital");
+		hashSet.add("Piyush");
+		hashSet.add("HashSetDemo");	
+		
+		System.out.println("Size: " + hashSet.size());
+		
+		
+		hashSet.add("Mital");
+		hashSet.add("Lists");
+		hashSet.remove("Lists");
+	
+		System.out.println("Contains Lists? " + hashSet.contains("Lists"));
+		Iterator<String> itr = hashSet.iterator();
+		
+		// Order of insertion not maintained while iterating through hashset
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
+	}
+}
