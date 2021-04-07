60660800e4d7cd3a5a9ecc198d95a1a3d7becb48
GLOBAL\pmital <piyushmital1996@gmail.com>
2021-04-07 22:24:42 +0530

    Longest Consecutive subsequence using O(N) time and O(N) aux space

---
+++ hashing/LongestConsecutiveSubsequence.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class LongestConsecutiveSubsequence{
+
+	public static int longestConsecutiveSubseq(Integer[] arr){
+		HashSet<Integer> set = new HashSet<Integer>(Arrays.asList(arr));
+		
+		int currSize = 0;
+		int maxSize = 0;
+		while(!set.isEmpty()){
+			Iterator<Integer> itr = set.iterator();
+			currSize = 1;
+			Integer item = itr.next();
+			Integer itemGt = item + 1, itemLt =  item - 1;
+			while(set.contains(itemGt) || set.contains(itemLt)){
+				if(set.contains(itemGt)){
+					set.remove(itemGt);
+					itemGt++;
+					currSize++;
+				}
+				if(set.contains(itemLt)){
+					set.remove(itemLt);
+					itemLt--;
+					currSize++;
+				}
+			}
+			set.remove(item);
+			maxSize = maxSize > currSize ? maxSize : currSize;
+		}
+		return maxSize;
+	}
+	
+	public static void main(String argsp[]){
+		Integer[] arr = {1, 9, 3, 4, 2, 20, 19, 22, 12, 21, 11, 44, 20, 18};
+		System.out.println(longestConsecutiveSubseq(arr));
+	}
+	
+}
